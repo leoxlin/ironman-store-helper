3f0d599ba0fca93b32f719d183212d41fbb22c9d
Leo Xuzhang Lin <me@leoxlin.com>
2023-06-15 19:50:37 -0400

    Rename class names

--- src/main/java/com/leoxlin/osrs/IronmanStoreHelperPlugin.java
+++ src/main/java/com/leoxlin/osrs/IronmanStoreHelperPlugin.java
@@ -14,15 +14,15 @@ import net.runelite.client.plugins.PluginDescriptor;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Example"
+	name = "Ironman Store Helper"
 )
-public class ExamplePlugin extends Plugin
+public class IronmanStoreHelperPlugin extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private IronmanStoreHelperConfig config;
 
 	@Override
 	protected void startUp() throws Exception
@@ -46,8 +46,8 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+	IronmanStoreHelperConfig provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(IronmanStoreHelperConfig.class);
 	}
 }
